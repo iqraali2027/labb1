50b42322be4960431ef9b2faa4fca73c3e10420b
iqraali2027 <h24iqral@du.se>
2024-12-04 17:57:54 +0100

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleApp3
+{
+    class Date
+    {
+        private int year;
+        private int month;
+        private int day;
+
+        public Date(int y, int m, int d)
+        {
+            year = y;
+            month = m;
+            day = d;
+        }
+        public override string ToString()
+        {
+            return year + "/" + month + "/" + day;
+        }
+/*        public int year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+        public int Month
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                month = value;
+            }
+        }
+        public int Day
+        {
+            get
+            {
+                return day;
+            }
+            set
+            {
+                day = value;
+            }
+        }
+*/
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date datum = new Date(2024, 12, 01);
+            Console.WriteLine(datum.ToString());
+
+        }
+    }
+}
